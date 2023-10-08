a55550ec2e001868e6c125153b2b215f090135e1
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-08 10:24:24 +0530

    Add files via upload

---
+++ ASSIG2.C
@@ -0,0 +1,17 @@
+/*Calculate prize of product*/
+main()
+{                                               
+
+int tbill,qpro,prize;
+clrscr();
+printf("\nEnter total bill of product:");
+scanf("%d",&tbill);
+printf("\nEnter product quantity:");
+scanf("%d",&qpro);
+prize=tbill/qpro;
+clrscr();
+printf("\nTotal bill of product: %d",tbill);
+printf("\nQuantity of product:  %d",qpro);
+printf("\nThe prize of a product: %d",prize);
+getch();
+}

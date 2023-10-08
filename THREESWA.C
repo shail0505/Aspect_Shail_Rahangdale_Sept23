f59476a1fbb754b4d7461faf32a2cd3a75fa2dbf
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-08 11:30:05 +0530

    Add files via upload

---
+++ THREESWA.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b,c,d;
+	clrscr();
+	printf("\nEnter three number:");
+	scanf("%d %d %d",&a,&b,&c);
+	printf("\nEntered three number is:%d %d %d",a,b,c);
+	if(a>b)
+	{
+		d=a;
+		a=b;
+		b=d;
+	}
+	if(b>c)
+	{
+		d=b;
+		b=c;
+		c=d;
+	}
+	if(a>b)
+	{
+		d=a;
+		a=b;
+		b=d;
+	}
+	if(b>c)
+	{
+		d=b;
+		b=c;
+		c=d;
+	}
+printf("\nAfter sorting sorted number is: %d %d %d",a,b,c);
+getch();
+}

1b83092895aaadf42cd532a79d0ac1b98ee96b27
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-13 09:32:28 +0530

    Add files via upload

---
+++ BACK1234.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k;
+	clrscr();
+	for(i=5;i>=1;i--)
+	{
+		for(j=5;j<=i;j--)
+		{
+
+			printf("");
+		}
+			for(k=1;k<=i;k++)
+			{
+
+				printf(" %d",k);
+			}
+		printf("\n");
+	}
+getch();
+}

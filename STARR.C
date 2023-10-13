81061878ff67f23c825b64e81312a7b05d3646bb
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-13 09:30:25 +0530

    Add files via upload

---
+++ STARR.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k;
+	clrscr();
+	for(i=1;i<=3;i++)
+	{
+		for(j=3;j>=i;j--)
+		{
+			printf(" ");
+		}
+			for(k=1;k<=i;k++)
+			{
+
+				printf(" * ");
+			}
+		printf("\n");
+	}
+getch();
+}

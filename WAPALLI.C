b43adca38749702e4c7b938ff1b98f45d5f01746
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-16 15:31:25 +0530

    Add files via upload

---
+++ WAPALLI.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void pal();
+void main()
+{
+	int a;
+	clrscr();
+	pal(a);
+	getch();
+}
+
+void pal(int n)
+{
+	int d1,rev=0,temp;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	temp=n;
+	for(;n>0;n=n/10)
+	{
+		d1=n%10;
+		rev=(rev*10)+d1;
+	}
+
+	if(rev==temp)
+	{
+		printf("Pallindrome number.");
+	}
+	else
+	{
+		printf("Not a pallindrome number.");
+	}
+}

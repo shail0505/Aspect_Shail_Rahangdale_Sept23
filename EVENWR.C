49aa66dd8a4bc7067022d7ad42ad0e90dd63fdc1
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-17 09:21:33 +0530

    Add files via upload

---
+++ EVENWR.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int evenodd();
+void main()
+{
+	int n;
+	char r;
+	clrscr();
+	r=evenodd();
+	if(r=='e')
+	{
+	  printf("Even number");
+	}
+	else
+	{
+	  printf("Odd number");
+	}
+	getch();
+}
+int evenodd()
+{
+	int a;
+	printf("Enter a number:");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		return 'e';
+
+	}
+	else
+	{
+		return 'o';
+
+	}
+}

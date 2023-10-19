e615d9a8eb4b7f6f2ea1f7a3c0c5737461a174ec
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-19 21:26:03 +0530

    Add files via upload

---
+++ COCWA.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int coc();
+void main()
+{
+	float c;
+	c=coc();
+	printf("Circumference of circle: %.2f",c);
+	getch();
+}
+int coc()
+{
+	int r;
+	float coc;
+	clrscr();
+	printf("\nEnter value for radius:");
+	scanf("%d",&r);
+	coc=2*3.14*r;
+	return coc;
+}

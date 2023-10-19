fd06c2736da76cd77c5ed574ed0b1cf556dd5f9f
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-19 21:05:31 +0530

    Add files via upload

---
+++ CUBB.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int vocub();
+void main()
+{
+	int a,b,h,c;
+	c=vocub(a,b,h);
+	printf("Volume of cuboid: %d",c);
+	getch();
+}
+int vocub(int l, int b, int h)
+{
+	int cuboid;
+	clrscr();
+	printf("\nEnter value for l,b and h:");
+	scanf("%d %d %d",&l,&b,&h);
+	cuboid=l*b*h;
+	return cuboid;
+}

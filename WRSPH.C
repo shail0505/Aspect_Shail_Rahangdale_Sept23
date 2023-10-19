d071085e68c791f85a71936f30cabd8aa750f931
shail0505 <134140603+shail0505@users.noreply.github.com>
2023-10-19 22:16:59 +0530

    Add files via upload

---
+++ WRSPH.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int vos();
+void main()
+{
+	float f;
+	f=vos();
+	printf("Volume of cube: %.2f",vos);
+	getch();
+}
+int vos()
+{
+	int r;
+	float vos;
+	clrscr();
+	printf("\nEnter value for r:");
+	scanf("%d",&r);
+	vos=1.3*3.14*r*r*r;
+	return vos;
+}
